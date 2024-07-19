e96311ecd9a96812993227003354efb29b782c1c
noyobo <noyobo@gmail.com>
2024-07-19 12:32:18 +0800

    Revert "fix: escape file path (#7)"
    This reverts commit cb5572861142538696532d24e36b557fa00125c8.

--- dist/index.js
+++ dist/index.js
@@ -31067,16 +31067,12 @@ const github_1 = __nccwpck_require__(5438);
 const exec_1 = __nccwpck_require__(1514);
 const node_fs_1 = __importDefault(__nccwpck_require__(7561));
 const ignore_1 = __importDefault(__nccwpck_require__(1230));
-const node_path_1 = __nccwpck_require__(9411);
 async function run() {
     const token = (0, core_1.getInput)('github_token');
     const prettierIgnore = (0, core_1.getInput)('prettier_ignore');
     const prettierVersion = (0, core_1.getInput)('prettier_version');
-    const fileExtensions = (0, core_1.getInput)('file_extensions');
     const github = (0, github_1.getOctokit)(token);
-    const commentIdentifier = '<!-- prettier-check-comment -->';
-    const fileExts = fileExtensions.split(',').map(ext => ext.trim());
-    async function getAllChangedFiles() {
+    const getAllChangedFiles = async () => {
         const changedFiles = [];
         let page = 1;
         // eslint-disable-next-line no-constant-condition
@@ -31088,53 +31084,67 @@ async function run() {
                 per_page: 100,
                 page
             });
-            if (files.length === 0)
+            if (files.length === 0) {
                 break;
-            changedFiles.push(...files.filter(f => f.status !== 'removed').map(f => f.filename));
+            }
+            const notDeletedFiles = files.filter(f => f.status !== 'removed');
+            changedFiles.push(...notDeletedFiles.map(f => f.filename));
             page++;
         }
-        return changedFiles.filter(f => {
-            const ext = (0, node_path_1.extname)(f);
-            return fileExts.includes(ext);
-        });
-    }
-    function filterFiles(files) {
-        if (node_fs_1.default.existsSync(prettierIgnore)) {
-            const ig = (0, ignore_1.default)().add(node_fs_1.default.readFileSync(prettierIgnore, 'utf-8'));
-            return files.filter(f => !ig.ignores(f));
-        }
-        return files;
-    }
-    function quote(args) {
-        return args.map(arg => arg.replace(/([~!#$^&*()\][{}|;'"<>?`\s])/g, '\\$1'));
-    }
+        return changedFiles;
+    };
     let changedFiles = await getAllChangedFiles();
-    changedFiles = filterFiles(changedFiles);
+    changedFiles = changedFiles.filter(f => /\.(js|jsx|ts|tsx|json|json5|css|less|scss|sass|html|md|mdx|vue)$/.test(f));
+    if (node_fs_1.default.existsSync(prettierIgnore)) {
+        const ig = (0, ignore_1.default)().add(node_fs_1.default.readFileSync(prettierIgnore, 'utf-8'));
+        changedFiles = changedFiles.filter(f => !ig.ignores(f));
+    }
+    const commentIdentifier = '<!-- prettier-check-comment -->';
     if (changedFiles.length === 0) {
         (0, core_1.info)('No files to check');
-        await updateComment(`${commentIdentifier}\nPrettier check passed! 🎉`);
+        const body = `${commentIdentifier}\nPrettier check passed! 🎉`;
+        const { data: comments } = await github.rest.issues.listComments({
+            owner: github_1.context.repo.owner,
+            repo: github_1.context.repo.repo,
+            issue_number: github_1.context.issue.number
+        });
+        const comment = comments.find(c => c.body.includes(commentIdentifier));
+        if (comment) {
+            await github.rest.issues.updateComment({
+                owner: github_1.context.repo.owner,
+                repo: github_1.context.repo.repo,
+                comment_id: comment.id,
+                body
+            });
+        }
     }
     else {
         (0, core_1.info)('Matched changed files:');
         (0, core_1.info)(changedFiles.map(f => `- ${f}`).join('\n'));
         await (0, exec_1.exec)('npm', ['install', '--global', `prettier@${prettierVersion}`]);
         let stderr = '';
-        const exitCode = await (0, exec_1.exec)('prettier', ['--check', ...changedFiles], {
+        const exitCode = await (0, exec_1.exec)('prettier', ['--check', ...changedFiles.map(f => encodeURI(f))], {
             ignoreReturnCode: true,
-            listeners: { stderr: data => (stderr += data.toString()) }
+            listeners: {
+                stderr: (data) => {
+                    stderr += data.toString();
+                }
+            }
         });
-        const prettierOutput = stderr.trim();
-        const prettierCommand = `npx prettier --write ${quote(prettierOutput.split('\n').map(line => line.trim().replace('[warn] ', ''))).join(' ')}`;
-        const body = exitCode === 0
-            ? `${commentIdentifier}\nPrettier check passed! 🎉`
-            : `${commentIdentifier}\n🚨 Prettier check failed for the following files:\n\n\`\`\`\n${prettierOutput}\n\`\`\`\n\nTo fix the issue, run the following command:\n\n\`\`\`\n${prettierCommand}\n\`\`\``;
-        await updateComment(body);
-        if (exitCode !== 0) {
-            (0, core_1.setFailed)('\nPrettier check failed 😢');
+        let body;
+        if (exitCode === 0) {
+            body = `${commentIdentifier}\nPrettier check passed! 🎉`;
         }
-        (0, core_1.setOutput)('exitCode', exitCode);
-    }
-    async function updateComment(body) {
+        else {
+            const prettierOutput = stderr;
+            const lines = prettierOutput.trim().split('\n');
+            lines.pop();
+            const prettierCommand = `npx prettier --write ${lines
+                .map(line => line.trim().replace('[warn] ', ''))
+                .map(f => encodeURI(f))
+                .join(' ')}`;
+            body = `${commentIdentifier}\n🚨 Prettier check failed for the following files:\n\n\`\`\`\n${prettierOutput.trim()}\n\`\`\`\n\nTo fix the issue, run the following command:\n\n\`\`\`\n${prettierCommand}\n\`\`\``;
+        }
         const { data: comments } = await github.rest.issues.listComments({
             owner: github_1.context.repo.owner,
             repo: github_1.context.repo.repo,
@@ -31157,6 +31167,13 @@ async function run() {
                 body
             });
         }
+        if (exitCode === 0) {
+            (0, core_1.info)('\nPrettier check passed 🎉');
+        }
+        else {
+            (0, core_1.setFailed)('\nPrettier check failed 😢');
+        }
+        (0, core_1.setOutput)('exitCode', 0);
     }
 }
 
@@ -31280,14 +31297,6 @@ module.exports = require("node:events");
 
 "use strict";
 module.exports = require("node:fs");
-
-/***/ }),
-
-/***/ 9411:
-/***/ ((module) => {
-
-"use strict";
-module.exports = require("node:path");
 
 /***/ }),
 
--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,15 @@ import { context, getOctokit } from '@actions/github'
 import { exec } from '@actions/exec'
 import fs from 'node:fs'
 import ignore from 'ignore'
-import { extname } from 'node:path'
 
 export async function run(): Promise<void> {
   const token = getInput('github_token')
   const prettierIgnore = getInput('prettier_ignore')
   const prettierVersion = getInput('prettier_version')
-  const fileExtensions = getInput('file_extensions')
+
   const github = getOctokit(token)
-  const commentIdentifier = '<!-- prettier-check-comment -->'
 
-  const fileExts = fileExtensions.split(',').map(ext => ext.trim())
-
-  async function getAllChangedFiles(): Promise<string[]> {
+  const getAllChangedFiles = async (): Promise<string[]> => {
     const changedFiles: string[] = []
     let page = 1
     // eslint-disable-next-line no-constant-condition
@@ -27,66 +23,83 @@ export async function run(): Promise<void> {
         per_page: 100,
         page
       })
-      if (files.length === 0) break
-      changedFiles.push(...files.filter(f => f.status !== 'removed').map(f => f.filename))
+      if (files.length === 0) {
+        break
+      }
+      const notDeletedFiles = files.filter(f => f.status !== 'removed')
+      changedFiles.push(...notDeletedFiles.map(f => f.filename))
       page++
     }
-    return changedFiles.filter(f => {
-      const ext = extname(f)
-      return fileExts.includes(ext)
-    })
-  }
-
-  function filterFiles(files: string[]): string[] {
-    if (fs.existsSync(prettierIgnore)) {
-      const ig = ignore().add(fs.readFileSync(prettierIgnore, 'utf-8'))
-      return files.filter(f => !ig.ignores(f))
-    }
-    return files
-  }
-
-  function quote(args: string[]): string[] {
-    return args.map(arg => arg.replace(/([~!#$^&*()\][{}|;'"<>?`\s])/g, '\\$1'))
+    return changedFiles
   }
 
   let changedFiles = await getAllChangedFiles()
-  changedFiles = filterFiles(changedFiles)
+
+  changedFiles = changedFiles.filter(f => /\.(js|jsx|ts|tsx|json|json5|css|less|scss|sass|html|md|mdx|vue)$/.test(f))
+
+  if (fs.existsSync(prettierIgnore)) {
+    const ig = ignore().add(fs.readFileSync(prettierIgnore, 'utf-8'))
+    changedFiles = changedFiles.filter(f => !ig.ignores(f))
+  }
+
+  const commentIdentifier = '<!-- prettier-check-comment -->'
 
   if (changedFiles.length === 0) {
     info('No files to check')
-    await updateComment(`${commentIdentifier}\nPrettier check passed! 🎉`)
+    const body = `${commentIdentifier}\nPrettier check passed! 🎉`
+    const { data: comments } = await github.rest.issues.listComments({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: context.issue.number
+    })
+
+    const comment = comments.find(c => c.body!.includes(commentIdentifier))
+
+    if (comment) {
+      await github.rest.issues.updateComment({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        comment_id: comment.id,
+        body
+      })
+    }
   } else {
     info('Matched changed files:')
     info(changedFiles.map(f => `- ${f}`).join('\n'))
     await exec('npm', ['install', '--global', `prettier@${prettierVersion}`])
 
     let stderr = ''
-    const exitCode = await exec('prettier', ['--check', ...changedFiles], {
+    const exitCode = await exec('prettier', ['--check', ...changedFiles.map(f => encodeURI(f))], {
       ignoreReturnCode: true,
-      listeners: { stderr: data => (stderr += data.toString()) }
+      listeners: {
+        stderr: (data: Buffer) => {
+          stderr += data.toString()
+        }
+      }
     })
 
-    const prettierOutput = stderr.trim()
-    const prettierCommand = `npx prettier --write ${quote(prettierOutput.split('\n').map(line => line.trim().replace('[warn] ', ''))).join(' ')}`
-    const body =
-      exitCode === 0
-        ? `${commentIdentifier}\nPrettier check passed! 🎉`
-        : `${commentIdentifier}\n🚨 Prettier check failed for the following files:\n\n\`\`\`\n${prettierOutput}\n\`\`\`\n\nTo fix the issue, run the following command:\n\n\`\`\`\n${prettierCommand}\n\`\`\``
-    await updateComment(body)
-
-    if (exitCode !== 0) {
-      setFailed('\nPrettier check failed 😢')
+    let body
+    if (exitCode === 0) {
+      body = `${commentIdentifier}\nPrettier check passed! 🎉`
+    } else {
+      const prettierOutput = stderr
+      const lines = prettierOutput.trim().split('\n')
+      lines.pop()
+      const prettierCommand = `npx prettier --write ${lines
+        .map(line => line.trim().replace('[warn] ', ''))
+        .map(f => encodeURI(f))
+        .join(' ')}`
+      body = `${commentIdentifier}\n🚨 Prettier check failed for the following files:\n\n\`\`\`\n${prettierOutput.trim()}\n\`\`\`\n\nTo fix the issue, run the following command:\n\n\`\`\`\n${prettierCommand}\n\`\`\``
     }
-    setOutput('exitCode', exitCode)
-  }
 
-  async function updateComment(body: string): Promise<void> {
     const { data: comments } = await github.rest.issues.listComments({
       owner: context.repo.owner,
       repo: context.repo.repo,
       issue_number: context.issue.number
     })
+
     const comment = comments.find(c => c.body!.includes(commentIdentifier))
+
     if (comment) {
       await github.rest.issues.updateComment({
         owner: context.repo.owner,
@@ -102,5 +115,12 @@ export async function run(): Promise<void> {
         body
       })
     }
+
+    if (exitCode === 0) {
+      info('\nPrettier check passed 🎉')
+    } else {
+      setFailed('\nPrettier check failed 😢')
+    }
+    setOutput('exitCode', 0)
   }
 }
